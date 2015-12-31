65461a80ff78379e2f3aa456e5bdf1b25ab8112a
leonelgpr33 <leonel.perez@mrci.com.mx>
2015-12-30 22:51:16 -0600

    Funcionalidad de Login telefónico con Fabric agredada.

--- Phone Register 2/AppDelegate.m
+++ Phone Register 2/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <Fabric/Fabric.h>
+#import <DigitsKit/DigitsKit.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Fabric with:@[[Digits class]]];
     return YES;
 }
 
